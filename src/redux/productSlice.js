b7bd7cf8a6ff91f5c67a03ed498f8613c13a2c28
Suraj Jadhav <105606282+surajjadhav29@users.noreply.github.com>
2024-12-14 16:22:09 +0530

    Add files via upload

---
+++ src/redux/productSlice.js
@@ -0,0 +1,27 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { Products } from './Products';
+
+const initialState = {
+products: Products
+};
+
+const productsSlice = createSlice({
+  name: 'products',
+  initialState,
+  reducers: {
+    addProduct: (state, action) => {
+      state.products.push(action.payload);
+    },
+    updateProduct: (state, action) => {
+      const index = state.products.findIndex(
+        (product) => product.id === action.payload.id
+      );
+      if (index !== -1) {
+        state.products[index] = action.payload;
+      }
+    },
+  },
+});
+
+export const { addProduct, updateProduct } = productsSlice.actions;
+export default productsSlice.reducer;
