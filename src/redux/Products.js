383730776253b6e011d2adf121eec106c7ec38dd
Suraj Jadhav <105606282+surajjadhav29@users.noreply.github.com>
2024-12-14 16:21:48 +0530

    Create Products.js

---
+++ src/redux/Products.js
@@ -0,0 +1,129 @@
+export const Products = [
+    {
+        "id": 1,
+        "name": "Wheat Flour",
+        "unit": "kg",
+        "category": "Finished",
+        "expiry": "2024-12-31",
+        "totalCost": 500,
+        "imageUrl": "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcT5sgaXpP4ORQYILXAhecRGLz1BrRDjE8PR8UEQLgaczu-_1X56f1qWz3FunsMVP9u9siV3I5kseMvoSFoL-yVIxObxozmcj_gjhebS5ZmoM5iCOeYhZYK8",
+        "materials": [
+            {
+                "id": 1,
+                "name": "Wheat",
+                "unit": "kg",
+                "quantity": 5,
+                "price": 80,
+                "tax": 20,
+                "totalAmount": 420
+            },
+            {
+                "id": 2,
+                "name": "Packaging Material",
+                "unit": "ltr",
+                "quantity": 2,
+                "price": 40,
+                "tax": 10,
+                "totalAmount": 90
+            }
+        ]
+    },
+    {
+        "id": 2,
+        "name": "Beverage Syrup",
+        "unit": "ltr",
+        "category": "Semi finished",
+        "expiry": "2025-01-15",
+        "totalCost": 300,
+        "imageUrl": "https://www.luckystore.in/cdn/shop/files/ZonePassionFruitFlavoredSyrup1050ml.png?v=1733145835",
+        "materials": [
+            {
+                "id": 1,
+                "name": "Sugar",
+                "unit": "kg",
+                "quantity": 10,
+                "price": 25,
+                "tax": 50,
+                "totalAmount": 300
+            }
+        ]
+    },
+    {
+        "id": 5,
+        "name": "Fruit Juice",
+        "unit": "ltr",
+        "category": "Finished",
+        "expiry": "2025-11-01",
+        "totalCost": 600,
+        "imageUrl": "https://images.indianexpress.com/2021/02/fruit-juice-1200.jpg",
+        "materials": [
+            {
+                "id": 1,
+                "name": "Fresh Oranges",
+                "unit": "kg",
+                "quantity": 8,
+                "price": 60,
+                "tax": 15,
+                "totalAmount": 510
+            },
+            {
+                "id": 2,
+                "name": "Sugar",
+                "unit": "kg",
+                "quantity": 3,
+                "price": 40,
+                "tax": 10,
+                "totalAmount": 150
+            }
+        ]
+    },
+    {
+        "id": 6,
+        "name": "Rice",
+        "unit": "kg",
+        "category": "Finished",
+        "expiry": "2025-05-30",
+        "totalCost": 700,
+        "imageUrl": "https://media.istockphoto.com/id/153737841/photo/rice.jpg?s=612x612&w=0&k=20&c=lfO7iLT0UsDDzra0uBOsN1rvr2d5OEtrG2uwbts33_c=",
+        "materials": [
+            {
+                "id": 1,
+                "name": "Rice",
+                "unit": "kg",
+                "quantity": 12,
+                "price": 50,
+                "tax": 30,
+                "totalAmount": 600
+            }
+        ]
+    },
+    {
+        "id": 7,
+        "name": "Tomato Sauce",
+        "unit": "ltr",
+        "category": "Finished",
+        "expiry": "2025-08-15",
+        "totalCost": 400,
+        "imageUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQHkbsai3DUWrV0AQugLA1cd_mUqLx8Rlvaqg&s",
+        "materials": [
+            {
+                "id": 1,
+                "name": "Tomatoes",
+                "unit": "kg",
+                "quantity": 10,
+                "price": 25,
+                "tax": 20,
+                "totalAmount": 250
+            },
+            {
+                "id": 2,
+                "name": "Sugar",
+                "unit": "kg",
+                "quantity": 5,
+                "price": 35,
+                "tax": 15,
+                "totalAmount": 175
+            }
+        ]
+    }
+]
